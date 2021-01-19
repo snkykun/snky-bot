f2314e29d1a6337389fcd28cd55b76b5e4d21730
Kyy <68399818+snkykun@users.noreply.github.com>
2021-01-19 16:06:06 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,17 +2,44 @@ import discord
 from discord.ext import commands
 emoji = '\N{THUMBS DOWN SIGN}'
 client = commands.Bot(command_prefix = '.')
-annoying = ()
+
+
+
+annoying = []
+@client.command()
+async def annoy(ctx, member : discord.Member):
+    annoying.append(member.id)
 @client.event
-async def on_ready():
-    print('Bot is ready')
+async def on_message(message):
+    if message.author.id in annoying:
+        await message.add_reaction(emoji)
+
+
+
+        
+# annoying = []
+# @client.command()
+# async def annoy(ctx, member : discord.Member):
+#     annoying.append(member.id)
+#     print(annoying)
+# @client.event
+# async def on_message(message):
+#     if message.author.id in annoying:
+#         await message.add_reaction(emoji)
+# @client.event
+# async def on_ready():
+#     print('Bot is ready')
+# @client.command()
+# async def annoy(ctx, member : discord.Member):
+#     await member.add_roles(annoying)
+# @client.event
+# async def on_message(message):
+#     if ('annoying') in message.author.roles:
+#         await message.add_reaction(emoji)
+
 
 @client.command()
-async def react(ctx, member : discord.Member):
-    reacting = member.id
-    @client.event
-    async def on_message(message):
-        if message.author.id == reacting:
-            await message.add_reaction(emoji)
+async def clear(ctx, amount=5):
+    await ctx.channel.purge()
 
 client.run('ODAxMTE5NzIxNDA3MTE5NDEx.YAcC4Q.qmAY0UNvK4lndiZh2knfeefKUc8')
