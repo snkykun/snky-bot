6c3a523e7206b5bcf1926b0c02fa4c0cc1a72c67
Kyy <68399818+snkykun@users.noreply.github.com>
2021-01-24 20:09:34 -0500

    Organized and labeled

--- bot.py
+++ bot.py
@@ -1,45 +1,57 @@
 import discord
+import pastas
+import emoji
 from discord.ext import commands
-emoji = '\N{THUMBS DOWN SIGN}'
 client = commands.Bot(command_prefix = '.')
 
-
-
-annoying = []
-@client.command()
-async def annoy(ctx, member : discord.Member):
-    annoying.append(member.id)
+## Setup
 @client.event
+async def on_ready():
+    print('Bot is ready')
+    custom = discord.Game('snky.cc')
+    await client.change_presence(status=discord.Status.online, activity=custom)
+
+## Come correction
+@client.listen()
 async def on_message(message):
-    if message.author.id in annoying:
-        await message.add_reaction(emoji)
+  str = message.content
+  if 'come' in str.lower() and message.author.id != 801119721407119411:
+    await message.add_reaction(:thumbs_down:)
+    await message.channel.send('*cum')
 
+## PASTAS
+@client.listen()
+async def on_message(message):
+  str = message.content
+  if 'goat' in str.lower() and message.author.id != 801119721407119411:
+    await message.channel.send(pastas.goat)
 
+@client.listen()
+async def on_message(message):
+  str = message.content
+  if 'innit' in str.lower() and message.author.id != 801119721407119411:
+    await message.channel.send(pastas.innit)
 
-        
-# annoying = []
-# @client.command()
-# async def annoy(ctx, member : discord.Member):
-#     annoying.append(member.id)
-#     print(annoying)
-# @client.event
-# async def on_message(message):
-#     if message.author.id in annoying:
-#         await message.add_reaction(emoji)
-# @client.event
-# async def on_ready():
-#     print('Bot is ready')
-# @client.command()
-# async def annoy(ctx, member : discord.Member):
-#     await member.add_roles(annoying)
-# @client.event
-# async def on_message(message):
-#     if ('annoying') in message.author.roles:
-#         await message.add_reaction(emoji)
+@client.listen()
+async def on_message(message):
+  str = message.content
+  if 'based' in str.lower() and message.author.id != 801119721407119411 and message.channel.is_nsfw():
+    await message.channel.send(pastas.based)
 
+@client.listen()
+async def on_message(message):
+  str = message.content
+  if 'simp' in str.lower() and message.author.id != 801119721407119411:
+    await message.channel.send(pastas.simp)
 
+@client.listen()
+async def on_message(message):
+  str = message.content
+  if 'pee' in str.lower() and message.author.id != 801119721407119411:
+    await message.channel.send(pastas.pee)
+## commands
 @client.command()
 async def clear(ctx, amount=5):
-    await ctx.channel.purge()
+    await ctx.channel.purge(limit=amount)
 
 client.run('ODAxMTE5NzIxNDA3MTE5NDEx.YAcC4Q.qmAY0UNvK4lndiZh2knfeefKUc8')
