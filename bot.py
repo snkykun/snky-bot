a1cd2d6c394288fe70ba494afa2d1fa4ea1d75bf
Kyy <68399818+snkykun@users.noreply.github.com>
2021-08-29 02:09:48 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -69,7 +69,7 @@ async def on_message(message):
                 await message.channel.send(file=File("./data/Retard-lf8zQN6agAw.mp4"))
         if 'mercy montage' in str.lower():
                 await message.channel.send(file=File("./data/MercyMontage-nMTj67b_Boc.mp4"))
-        if 'peak' in str.lower():
+        if ' peak' in str.lower():
                 await message.channel.send(file=File("./data/KW2fX8VRPVwcPYUC.mp4"))
         if 'cock' in str.lower():
             await message.channel.send('and balls (never forget the balls.)')
